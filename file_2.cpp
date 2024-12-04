5978d0b4c97a1f47688bb580a247937382f1b560
Vlad Perepelytsyn <vladperep58@gmail.com>
2024-12-04 20:30:30 +0200

    add file_2.cpp

---
+++ file_2.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <iomanip>
+#include <windows.h>
+#include <math.h>
+#include <stdio.h>
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	// Площа клумби: S=3.14*r*r; Периметр клумби: P = 2*3.14*r; r - радіус
+
+	/*
+	int r = 5;
+	double S, P;
+	S = 3.14* r * r;
+	P = 2 * 3.14 * r;
+	cout << "S=" << S << '\t' << "P=" << P << endl;
+	*/
+
+	/*
+	int r;
+	double S, P;
+	cout << "Введідь радіус клумби r:";
+	cin >> r;
+	S = 3.14 * r * r;
+	P = 2 * 3.14 * r;
+	cout << "S=" << S << '\t' << "P=" << P << endl;
+	*/
+	
+	/*
+	int r = 5;
+	double S, P;
+	S = 3.14 * r * r;
+	P = 2 * 3.14 * r;
+	printf("S = %lf P = %lf", S, P);
+	*/
+
+	
+	int r;
+	double S, P;
+	printf("Введідь радіус клумби r:");
+	scanf_s("%d", &r);
+	S = 3.14 * r * r;
+	P = 2 * 3.14 * r;
+	printf("S = %lf P = %lf", S, P);
+	
+
+	return 0;
+
+
+}
